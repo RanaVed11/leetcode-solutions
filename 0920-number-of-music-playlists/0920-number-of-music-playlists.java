1534fea6f7b02e1900d76f3cb0ac098aea75f79d
Rana Ved <ranaved264@gmail.com>
2026-06-29 21:33:30 +0530

    Time: 8 ms (9.68%), Space: 43.9 MB (62.37%) - LeetHub

---
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numMusicPlaylists(int n, int goal, int k) {
+        long[][] dp = new long[goal + 1][n + 1];
+        dp[0][0] = 1;
+        long mod = 1000000007;
+        for (int i = 1; i <= goal; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = (dp[i - 1][j - 1] * (n - (j - 1))) % mod;
+                dp[i][j] = (dp[i][j] + dp[i - 1][j] * Math.max(j - k, 0)) % mod;
+            }
+        }
+        return (int) dp[goal][n];
+    }
+}
