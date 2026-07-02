7a6c261d5a02eb5974981ee5df726bc5e596492a
Rana Ved <ranaved264@gmail.com>
2026-07-02 20:54:27 +0530

    Time: 39 ms (9.04%), Space: 46.4 MB (58.57%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maxPoints(int[][] points) {
+        if (points.length <= 2) return points.length;
+        int max = 0;
+        
+        for (int i = 0; i < points.length; i++) {
+            Map<String, Integer> map = new HashMap<>();
+            int duplicate = 1;
+            int localMax = 0;
+            
+            for (int j = i + 1; j < points.length; j++) {
+                int dx = points[j][0] - points[i][0];
+                int dy = points[j][1] - points[i][1];
+                
+                if (dx == 0 && dy == 0) {
+                    duplicate++;
+                    continue;
+                }
+                
+                int gcd = gcd(dx, dy);
+                dx /= gcd;
+                dy /= gcd;
+                
+                String slope = dx + "/" + dy;
+                map.put(slope, map.getOrDefault(slope, 0) + 1);
+                localMax = Math.max(localMax, map.get(slope));
+            }
+            
+            max = Math.max(max, localMax + duplicate);
+        }
+        
+        return max;
+    }
+    
+    private int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+}
