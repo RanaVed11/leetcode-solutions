2c23ee33e3932da161ebdc595b2104ea7f551886
Rana Ved <ranaved264@gmail.com>
2026-07-09 09:19:53 +0530

    Time: 123 ms (10.89%), Space: 79.3 MB (45.67%) - LeetHub

---
+++ 2631-group-by/2631-group-by.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    const res = {};
+    for (const item of this) {
+        const key = fn(item);
+        if (!(key in res)) {
+            res[key] = [];
+        }
+        res[key].push(item);
+    }
+    return res;
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
