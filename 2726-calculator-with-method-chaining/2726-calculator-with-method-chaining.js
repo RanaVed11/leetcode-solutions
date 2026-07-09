9c93b65d30fc5ea13ad54d526299a84d2fb5a83b
Rana Ved <ranaved264@gmail.com>
2026-07-09 09:21:50 +0530

    Time: 37 ms (84.75%), Space: 53.2 MB (77.81%) - LeetHub

---
+++ 2726-calculator-with-method-chaining/2726-calculator-with-method-chaining.js
@@ -0,0 +1,37 @@
+class Calculator {
+    constructor(value) {
+        this.value = value;
+    }
+
+    add(num) {
+        this.value += num;
+        return this;
+    }
+
+    subtract(num) {
+        this.value -= num;
+        return this;
+    }
+
+    multiply(num) {
+        this.value *= num;
+        return this;
+    }
+
+    divide(num) {
+        if (num === 0) {
+            throw new Error("Division by zero is not allowed");
+        }
+        this.value /= num;
+        return this;
+    }
+
+    power(num) {
+        this.value = this.value ** num;
+        return this;
+    }
+
+    getResult() {
+        return this.value;
+    }
+}
