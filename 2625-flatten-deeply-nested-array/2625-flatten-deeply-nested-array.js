a7112fa16d9be5781e17c3a7d2aed5bfef30ae47
Rana Ved <ranaved264@gmail.com>
2026-07-09 09:20:38 +0530

    Time: 76 ms (78.25%), Space: 75 MB (69.03%) - LeetHub

---
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -0,0 +1,16 @@
+var flat = function(arr, n) {
+    let result = [];
+
+    function flatten(current, depth) {
+        for (let item of current) {
+            if (Array.isArray(item) && depth > 0) {
+                flatten(item, depth - 1);
+            } else {
+                result.push(item);
+            }
+        }
+    }
+
+    flatten(arr, n);
+    return result;
+};
